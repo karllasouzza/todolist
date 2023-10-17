01f069efaa54ad89027cf3304bfd52d1fefed51a
karlla souzza <karlla.souzza7@gmail.com>
2023-10-16 23:14:31 -0300

    feat: Add the update method.

--- src/main/java/br/com/karlla/souzza/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/karlla/souzza/todolist/filter/FilterTaskAuth.java
@@ -25,7 +25,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
       throws ServletException, IOException {
 
     var servletPath = request.getServletPath();
-    if (servletPath.equals("/tasks/")) {
+    if (servletPath.startsWith("/tasks/")) {
       var authorization = request.getHeader("Authorization");
       var authEncoded = authorization.substring("Basic".length()).trim();
 
--- src/main/java/br/com/karlla/souzza/todolist/task/TaskController.java
+++ src/main/java/br/com/karlla/souzza/todolist/task/TaskController.java
@@ -7,11 +7,15 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.karlla.souzza.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -39,8 +43,17 @@ public class TaskController {
     return ResponseEntity.status(HttpStatus.OK).body(task);
   }
 
+  @GetMapping("/")
   public List<TaskModel> list(HttpServletRequest request) {
     var idUser = request.getAttribute("idUser");
     return this.taskRepository.findByIdUser((UUID) idUser);
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var task = this.taskRepository.findById(id).orElse(null);
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
