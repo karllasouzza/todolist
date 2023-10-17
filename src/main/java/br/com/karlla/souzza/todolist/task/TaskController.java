1712b4da6eaa2f94cdcd290f61acf79fe87d6cc5
karlla souzza <karlla.souzza7@gmail.com>
2023-10-16 23:54:44 -0300

    feat: Create the Exception Handle

--- src/main/java/br/com/karlla/souzza/todolist/task/TaskController.java
+++ src/main/java/br/com/karlla/souzza/todolist/task/TaskController.java
@@ -50,10 +50,22 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
     var task = this.taskRepository.findById(id).orElse(null);
+
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Tarefa não encontrada!");
+    }
+
+    var idUser = request.getAttribute("idUser");
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("Usuário não ter permição para alterar esta tarefa!");
+    }
+
     Utils.copyNonNullProperties(taskModel, task);
 
-    return this.taskRepository.save(task);
+    return ResponseEntity.ok().body(this.taskRepository.save(task));
   }
 }
